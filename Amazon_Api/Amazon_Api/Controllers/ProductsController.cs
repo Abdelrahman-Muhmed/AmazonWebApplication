e43d04a16dd7f782f3ac92af9ad8b67451102fd4
Abdelrahman-Muhmed <abdogomaa18@yahoo.com>
2024-08-09 12:29:51 +0300

    getProductAndBrand

--- Amazon_Api/Amazon_Api/Controllers/ProductsController.cs
+++ Amazon_Api/Amazon_Api/Controllers/ProductsController.cs
@@ -11,11 +11,18 @@ namespace Amazon_Api.Controllers
     public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _Products;
+        private readonly IGenericRepository<ProductBrand> _Brand;
+        private readonly IGenericRepository<ProductCategory> _category;
         private readonly IMapper _mapper;
-        public ProductsController(IGenericRepository<Product> Products , IMapper mapper)
+        public ProductsController(IGenericRepository<Product> Products
+            , IGenericRepository<ProductBrand> Brand
+            , IGenericRepository<ProductCategory> category
+            , IMapper mapper)
         {
             _Products = Products;
             _mapper = mapper;
+            _Brand = Brand;
+            _category = category;
         }
 
         [HttpGet]
@@ -38,6 +45,20 @@ namespace Amazon_Api.Controllers
             return Ok(ProductMapp);
 
         }
-    
+
+        [HttpGet("getBrand")]
+        public async Task<ActionResult<ProductBrand>> getProductPrand()
+        {
+            var brands = await _Brand.GetAllAsync();
+            return Ok(brands);
+        }
+        [HttpGet("getCategory")]
+        public async Task<ActionResult<ProductBrand>> getProductCategory()
+        {
+            var categors  = await _category.GetAllAsync();
+            return Ok(categors);
+
+        }
+
     }
 }
